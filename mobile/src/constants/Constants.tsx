9e0a9bfa6f6e651d09a6761cbaa832254b6e20fb
Higor <higor-sj@hotmail.com>
2020-11-27 02:54:08 -0300

    update ip server aws

--- mobile/src/constants/Constants.tsx
+++ mobile/src/constants/Constants.tsx
@@ -3,7 +3,7 @@ import { numbers } from './numbers'
 const Constants = {
     ...numbers,
     // baseURL: 'http://192.168.0.120:3000', // local
-    baseURL: 'http://3.92.26.142:3000', // aws instance
+    baseURL: 'http://54.227.199.11:3000', // aws instance
 }
 
 export default Constants
